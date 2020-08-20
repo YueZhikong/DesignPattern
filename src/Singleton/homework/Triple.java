9f27edac95dae86d785582f299a590a2852b2e53
sunyanyang <sunyanyang@ebupt.com>
2020-08-20 16:25:25 +0800

    chapter05

--- src/Singleton/homework/Triple.java
+++ src/Singleton/homework/Triple.java
@@ -1,6 +1,18 @@
 package Singleton.homework;
 
+import java.util.ArrayList;
+
 public class Triple {
-    
-    private Triple(){};
+    private static Triple[] triples = new Triple[]{new Triple(),new Triple(),new Triple()};
+    private Triple(){}
+
+    public static Triple getInstance(int id) {
+        return triples[id];
+    }
+
+    public static void main(String[] args) {
+        Triple one = Triple.getInstance(0);
+        Triple two = Triple.getInstance(1);
+        System.out.println(one == two);
+    }
 }
